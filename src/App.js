cc22a95e6ab7edb0b87a5425f48ed415f92d71b6
imlakshaychauhan <imlakshaychauhan@gmail.com>
2022-07-22 18:42:26 +0530

    Create User

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import { db } from "./firebase-config";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 
 const App = () => {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
   const [users, setUsers] = useState([]);
   const userCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(userCollectionRef, {name: newName, age: newAge});
+  };
+
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(userCollectionRef);
@@ -15,6 +22,20 @@ const App = () => {
 
   return (
     <div>
+      <input
+        placeholder="Enter Name"
+        onChange={(e) => {
+          setNewName(e.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Enter Age"
+        onChange={(e) => {
+          setNewAge(e.target.value);
+        }}
+      />
+      <button onClick={createUser}>Create User</button>
       {users.map((user) => {
         return (
           <div>
